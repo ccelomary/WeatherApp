ffc7ae4e12346570fe1500d552693cb5f27489b7
Mohamed Elomary <elomarymohamed72@gmail.com>
2022-03-06 17:32:36 +0100

    finish

--- src/Components/left_search_detail/Services/GetWeatherByCity.js
+++ src/Components/left_search_detail/Services/GetWeatherByCity.js
@@ -3,7 +3,7 @@ import { GET_WEATHER_BY_LOCATION } from "../../../reducers/LocationReducers";
 
 export default function GetWeatherByCity(woid){
     return async (dispatch)=>{
-        const data = await (await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woid}/`)).json();
+        const data = await (await fetch(`api/location/${woid}/`)).json();
         return dispatch({type: GET_WEATHER_BY_LOCATION, data: data})
     }
 }
--- src/Content.jsx
+++ src/Content.jsx
@@ -38,14 +38,14 @@ const getWeather = ()=>{
         let positionInfo;
         if (position)
         {
-            positionInfo = await (await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?lattlong=${position[0]},${position[1]}`)).json();
+            positionInfo = await (await fetch(`api/location/search/?lattlong=${position[0]},${position[1]}`)).json();
             positionInfo = positionInfo[0];
         }
         else {
-            positionInfo = await (await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=london`)).json();
+            positionInfo = await (await fetch(`api/location/search/?query=london`)).json();
             positionInfo = positionInfo[0];
         }
-        const data = await (await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${positionInfo.woeid}/`)).json();
+        const data = await (await fetch(`api/location/${positionInfo.woeid}/`)).json();
         return dispatch({type: GET_WEATHER_BY_LOCATION, data:data});
     };
 }
